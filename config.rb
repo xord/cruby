369700aebad1e7a15829798a5d3f389f770f7139
xordog <xordog@gmail.com>
2018-10-30 02:40:33 +0900

    ruby 2.3.8.

--- config.rb
+++ config.rb
@@ -5,8 +5,8 @@ POD_VERSION = 0
 
 GITHUB_URL  = "https://github.com/xord/cruby"
 
-RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.7.tar.gz'
-RUBY_SHA256 = '35cd349cddf78e4a0640d28ec8c7e88a2ae0db51ebd8926cd232bb70db2c7d7f'
+RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.8.tar.gz'
+RUBY_SHA256 = 'b5016d61440e939045d4e22979e04708ed6c8e1c52e7edb2553cf40b73c59abf'
 
 
 module CRuby
