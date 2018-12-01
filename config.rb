6ed91a84da51347d819bee54c6acded086d469b4
xordog <xordog@gmail.com>
2018-12-01 10:43:55 +0900

    ruby 2.6.0-preview3

--- config.rb
+++ config.rb
@@ -5,8 +5,8 @@ POD_VERSION = 0
 
 GITHUB_URL  = "https://github.com/xord/cruby"
 
-RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.8.tar.gz'
-RUBY_SHA256 = 'b5016d61440e939045d4e22979e04708ed6c8e1c52e7edb2553cf40b73c59abf'
+RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.0-preview3.tar.gz'
+RUBY_SHA256 = '60243e3bd9661e37675009ab66ba63beacf5dec748885b9b93916909f965f27a'
 
 
 module CRuby
@@ -16,7 +16,7 @@ module CRuby
   end
 
   def self.ruby_version ()
-    m = RUBY_URL.match /ruby\-(\d)\.(\d)\.(\d)\.tar\.gz/
+    m = RUBY_URL.match /ruby\-(\d)\.(\d)\.(\d)(?:\-\w*)?\.tar\.gz/
     raise "invalid ruby version" unless m && m.captures.size == 3
     m.captures.map &:to_i
   end
