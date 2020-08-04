84b6bf31ebdd20dea66cb1f8289de182077ab19d
xordog <xordog@gmail.com>
2020-08-04 23:16:30 +0900

    ruby 2.7.0

--- config.rb
+++ config.rb
@@ -1,12 +1,12 @@
 # -*- mode: ruby; coding: utf-8 -*-
 
 
-POD_VERSION = 2
+POD_VERSION = 0
 
 GITHUB_URL  = "https://github.com/xord/cruby"
 
-RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.6.tar.gz'
-RUBY_SHA256 = '364b143def360bac1b74eb56ed60b1a0dca6439b00157ae11ff77d5cd2e92291'
+RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.0.tar.gz'
+RUBY_SHA256 = '8c99aa93b5e2f1bc8437d1bbbefd27b13e7694025331f77245d0c068ef1f8cbe'
 
 OSSL_URL    = 'https://www.openssl.org/source/openssl-1.1.1c.tar.gz'
 OSSL_SHA256 = 'f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f4c90'
