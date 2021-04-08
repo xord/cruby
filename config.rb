5217ea600c85fa062178981c6b35328542773bed
xordog <xordog@gmail.com>
2021-04-08 23:44:53 +0900

    ruby 2.7.3

--- config.rb
+++ config.rb
@@ -5,8 +5,8 @@ POD_VERSION = 0
 
 GITHUB_URL  = "https://github.com/xord/cruby"
 
-RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.2.tar.gz'
-RUBY_SHA256 = '6e5706d0d4ee4e1e2f883db9d768586b4d06567debea353c796ec45e8321c3d4'
+RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.3.tar.gz'
+RUBY_SHA256 = '8925a95e31d8f2c81749025a52a544ea1d05dad18794e6828709268b92e55338'
 
 OSSL_URL    = 'https://www.openssl.org/source/openssl-1.1.1i.tar.gz'
 OSSL_SHA256 = 'e8be6a35fe41d10603c3cc635e93289ed00bf34b79671a3a4de64fcee00d5242'
