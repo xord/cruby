f7fed9c3137fc8dc4e7d6875cb09be7041095e52
xordog <xordog@gmail.com>
2022-03-24 00:25:24 +0900

    update openssl to 1.1.1n

--- config.rb
+++ config.rb
@@ -8,8 +8,8 @@ GITHUB_URL  = "https://github.com/xord/cruby"
 RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/3.1/ruby-3.1.0.tar.gz'
 RUBY_SHA256 = '50a0504c6edcb4d61ce6b8cfdbddaa95707195fab0ecd7b5e92654b2a9412854'
 
-OSSL_URL    = 'https://www.openssl.org/source/openssl-1.1.1m.tar.gz'
-OSSL_SHA256 = 'f89199be8b23ca45fc7cb9f1d8d3ee67312318286ad030f5316aca6462db6c96'
+OSSL_URL    = 'https://www.openssl.org/source/openssl-1.1.1n.tar.gz'
+OSSL_SHA256 = '40dceb51a4f6a5275bde0e6bf20ef4b91bfc32ed57c0552e2e8e15463372b17a'
 
 
 module CRuby
