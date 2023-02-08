75a98b38f9b7b61a1ad5d2048bee8c360185f331
xordog <xordog@gmail.com>
2023-02-09 02:35:15 +0900

    ruby 3.2.1

--- config.rb
+++ config.rb
@@ -1,12 +1,12 @@
 # -*- mode: ruby; coding: utf-8 -*-
 
 
-POD_VERSION = 52
+POD_VERSION = 0
 
 GITHUB_URL  = "https://github.com/xord/cruby"
 
-RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.0.tar.gz'
-RUBY_SHA256 = 'daaa78e1360b2783f98deeceb677ad900f3a36c0ffa6e2b6b19090be77abc272'
+RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.1.tar.gz'
+RUBY_SHA256 = '13d67901660ee3217dbd9dd56059346bd4212ce64a69c306ef52df64935f8dbd'
 
 OSSL_URL    = 'https://www.openssl.org/source/openssl-3.0.8.tar.gz'
 OSSL_SHA256 = '6c13d2bf38fdf31eac3ce2a347073673f5d63263398f1f69d0df4a41253e4b3e'
