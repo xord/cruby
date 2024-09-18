3c86dee152fb272944386501d9a9438385babf31
xordog <xordog@gmail.com>
2024-09-19 01:53:36 +0900

    OpenSSL 3.3.2

--- config.rb
+++ config.rb
@@ -1,15 +1,15 @@
 # -*- mode: ruby -*-
 
 
-POD_VERSION = 4
+POD_VERSION = 5
 
 GITHUB_URL  = "https://github.com/xord/cruby"
 
 RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.2.tar.gz'
 RUBY_SHA256 = '96c57558871a6748de5bc9f274e93f4b5aad06cd8f37befa0e8d94e7b8a423bc'
 
-OSSL_URL    = 'https://www.openssl.org/source/openssl-3.1.3.tar.gz'
-OSSL_SHA256 = 'f0316a2ebd89e7f2352976445458689f80302093788c466692fb2a188b2eacf6'
+OSSL_URL    = 'https://github.com/openssl/openssl/releases/download/openssl-3.3.2/openssl-3.3.2.tar.gz'
+OSSL_SHA256 = '2e8a40b01979afe8be0bbfb3de5dc1c6709fedb46d6c89c10da114ab5fc3d281'
 
 YAML_URL    = 'https://github.com/yaml/libyaml/releases/download/0.2.5/yaml-0.2.5.tar.gz'
 
