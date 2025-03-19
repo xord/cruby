8a58127323b7d35441e061fc9c397908a9323d6c
xordog <xordog@gmail.com>
2025-03-20 00:08:00 +0900

    Ruby 3.3.7

--- config.rb
+++ config.rb
@@ -5,8 +5,8 @@ POD_VERSION = 0
 
 GITHUB_URL  = "https://github.com/xord/cruby"
 
-RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.6.tar.gz'
-RUBY_SHA256 = '8dc48fffaf270f86f1019053f28e51e4da4cce32a36760a0603a9aee67d7fd8d'
+RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.7.tar.gz'
+RUBY_SHA256 = '9c37c3b12288c7aec20ca121ce76845be5bb5d77662a24919651aaf1d12c8628'
 
 OSSL_URL    = 'https://github.com/openssl/openssl/releases/download/openssl-3.4.0/openssl-3.4.0.tar.gz'
 OSSL_SHA256 = 'e15dda82fe2fe8139dc2ac21a36d4ca01d5313c75f99f46c4e8a27709b7294bf'
