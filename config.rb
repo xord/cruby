64fbd174852cb64bc63fb7705ec7f9c49f663ce2
xordog <xordog@gmail.com>
2026-05-01 14:15:01 +0900

    cruby: Revert to Ruby 3.4.0 for sequential 3.4.x releases

--- config.rb
+++ config.rb
@@ -5,8 +5,8 @@ POD_VERSION = 0
 
 GITHUB_URL  = "https://github.com/xord/cruby"
 
-RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/3.4/ruby-3.4.2.tar.gz'
-RUBY_SHA256 = '41328ac21f2bfdd7de6b3565ef4f0dd7543354d37e96f157a1552a6bd0eb364b'
+RUBY_URL    = 'https://cache.ruby-lang.org/pub/ruby/3.4/ruby-3.4.0.tar.gz'
+RUBY_SHA256 = '068c8523442174bd3400e786f4a6952352c82b1b9f6210fd17fb4823086d3379'
 
 OSSL_URL    = 'https://github.com/openssl/openssl/releases/download/openssl-3.4.1/openssl-3.4.1.tar.gz'
 OSSL_SHA256 = '002a2d6b30b58bf4bea46c43bdd96365aaf8daa6c428782aa4feee06da197df3'
